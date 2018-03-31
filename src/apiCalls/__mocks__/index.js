9b4860eebbbc046e68c327b873b223b2c3cd32bd
Jon Sweet <jonathan.h.sweet@gmail.com>
2018-03-31 10:40:43 -0600

    Update tests and lint codebase

--- src/apiCalls/__mocks__/index.js
+++ src/apiCalls/__mocks__/index.js
@@ -1,12 +1,13 @@
+/*eslint-disable id-blacklist*/
 export const signIn = jest.fn().mockImplementationOnce(() => 
   Promise.reject({}))
-    .mockImplementation(() => Promise.resolve({
-      status: 'success',
-      data: {
-        email: 'taco@taco',
-        name: 'taco',
-        id: 2,
-        password: 'taco'
-      },
-      message: 'Retrieved ONE User'
-    }));
+  .mockImplementation(() => Promise.resolve({
+    status: 'success',
+    data: {
+      email: 'taco@taco',
+      name: 'taco',
+      id: 2,
+      password: 'taco'
+    },
+    message: 'Retrieved ONE User'
+  }));
